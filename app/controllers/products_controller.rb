0faa8d4cfea2ccb78884e94365f1cd3e60abed72
Andreza Feu <andrezafeu@gmail.com>
2016-08-02 23:52:53 -0400

    Add new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+	before_action :authenticate_user!, only: [:new]
+	def index
+	end
+	def new
+		@product = Product.new
+	end
+	def create
+		@product = Product.new(
+			:title => params[:product][:title],
+			:description => params[:product][:description],
+			:bidding => params[:product][:bidding]
+			)
+		@product.id = current_user.id
+		if @product.save
+			flash[:notice] = "You product was saved."
+			redirect_to products_path
+		else
+			render "new"
+		end
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+	validates_presence_of :title, :bidding
+	belongs_to :user
 end
